5b59f8429d84698564e8ab1c41bd41a8ec8a660c
PJRDANTAS <PJRDANTAS@users.noreply.github.com>
2026-05-02 05:25:47 -0300

    RETIRANDO PASTAS DESNECESSARIAS

--- projeto-piloto-app/host/src/app/models/auth-request.model.ts
+++
@@ -1,9 +0,0 @@
-export interface AuthRequest {
-  nome: string;
-  login: string;
-  senha: string;
-  ativo: 'S' | 'N';
-  perfisIds?: number[];
-  rolesIds?: number[];
-  permissionsIds?: number[];
-}
--- projeto-piloto-app/host/src/app/models/perfil-request.model.ts
+++
@@ -1,4 +0,0 @@
-export interface PerfilRequest {
-  nmPerfil: string;
-  permissoesIds: number[]; // Apenas os IDs para o backend vincular
-}
